be8e59a820c7385bac93ddbb5cba95d3e24928ca
SaigyoujiYukon <misaka09983@gmail.com>
2021-12-22 17:03:02 +0800

    none

--- src/xyz/saigyoujiyukon/converter/Research.java
+++ src/xyz/saigyoujiyukon/converter/Research.java
@@ -1,9 +1,10 @@
 package xyz.saigyoujiyukon.converter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import static java.awt.TextArea.SCROLLBARS_NONE;
 
 public class Research {
     public static void main(String[] args) {
@@ -11,31 +12,50 @@ public class Research {
     }
 }
 class ResearchFrame extends Frame{
+
+    TextArea inputText,outputText;
+
     public ResearchFrame() {
         setTitle("科研过滤器转换");
         setSize(1270, 720);
-        setVisible(true);
+        setResizable(false);
 //        setLayout(new BorderLayout(1, 2));
         Button button = new Button("转换");
 
+
         Panel topPanel = new Panel();
+        Label topTitle = new Label("科研过滤器转换");
         add(topPanel,BorderLayout.NORTH);
-        topPanel.setSize(200,100);
+        topPanel.setSize(20,30);
+        topPanel.add(topTitle);
+        topPanel.setBackground(Color.red);
 
-        TextArea inputText = new TextArea();
-        TextArea outputText = new TextArea();
+        Panel mainPanel = new Panel();
+        TextArea inputText = new TextArea("",30,70,TextArea.SCROLLBARS_NONE);
+        TextArea outputText = new TextArea("",30,70,TextArea.SCROLLBARS_NONE);
         Label label = new Label("====>");
-        Panel panel = new Panel();
-        Panel textPanel1 = new Panel();
-        Panel textPanel2 = new Panel();
+        Panel inputTextPanel = new Panel();
+        Panel outputTextPanel = new Panel();
+        Panel downPanel = new Panel();
 
-        add(panel);
-        panel.setLayout(new FlowLayout());
-        textPanel1.add(inputText);
-        panel.add(label);
-        textPanel2.add(outputText);
-        add(button,BorderLayout.SOUTH);
+        add(mainPanel);
+        mainPanel.setLayout(new GridLayout(1,2));
+//        panel.setLayout(new FlowLayout());
+        inputTextPanel.add(inputText);
+        outputTextPanel.add(outputText);
+        mainPanel.add(inputTextPanel);
+//        panel.add(label);
+//        add(label,BorderLayout.CENTER);
+        mainPanel.add(outputTextPanel);
+        add(downPanel,BorderLayout.SOUTH);
+        downPanel.setBounds(400,400,50,50);
+        downPanel.add(button);
+//        downPanel.setSize(20,90);
+        button.addActionListener(new ConversionProcess());
 
+
+        //显示窗口
+        setVisible(true);
         //退出
         addWindowListener(new WindowAdapter() {
             @Override
@@ -43,5 +63,17 @@ class ResearchFrame extends Frame{
                 System.exit(0);
             }
         });
+    }
+
+    private class ConversionProcess implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String input = inputText.getText();
+            String output;
+            output = input;
+            outputText.setText(output);
+
+        }
     }
 }
